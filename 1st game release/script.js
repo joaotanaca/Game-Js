98194cb0f36af621360c3786d83ea1b2c9e4e7d8
joaotanaca <jtanacar@gmail.com>
2020-01-03 13:37:55 +0000

    Atualizações canvas

---
+++ 1st game release/script.js
@@ -0,0 +1,11 @@
+const screen = document.getElementById('screen')
+const context = screen.getContext('2d')
+
+const color = 'red'
+const positionX= 0
+const positionY= 0
+const width = 250
+const height = 250 
+
+context.fillStyle = color
+context.fillRect(positionX, positionY, width, height)
